382aa9f7f993f8a7ba608f9705253453259c6d5c
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-15 00:08:43 -0300

    15AGO24 - App Orçamento Pessoal - Criando funções para controlar os filtros de consulta

--- app.js
+++ app.js
@@ -93,6 +93,12 @@ class Bd {
 
     }
 
+    pesquisar (despesa) {
+
+        console.log(despesa)
+
+    }
+
 }
 
 let bd = new Bd()
@@ -250,5 +256,20 @@ function limparFormulario () {
    tipo.value = ''
    descricao.value = ''
    valor.value = ''
+
+}
+
+function pesquisarDespesa () {
+
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+    bd.pesquisar(despesa)
 
 }
