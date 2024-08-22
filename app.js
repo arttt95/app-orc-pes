079cf69071716c3bd7962cf4b1965cc56c9adb01
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-22 10:54:01 -0300

    22AGO24 - App Orçamento Pessoal - Adicionando botão de romover despesa na lista de consulta (criação de id para cada despesa). PROJETO FINALIZADO!

--- app.js
+++ app.js
@@ -79,11 +79,10 @@ class Bd {
 
                 continue
 
-            } else {
-
-                despesas.push(despesa)
-
             }
+
+            despesa.id = i
+            despesas.push(despesa)
 
             
 
@@ -156,6 +155,12 @@ class Bd {
 
         return despesasFiltradas
 
+
+    }
+
+    remover (id) {
+
+        localStorage.removeItem(id)
 
     }
 
@@ -270,8 +275,6 @@ function carregaListaDespesas (despesas = [], filtro = false) {
 
     // Percorrer o array 'despesas' listando cada despesa de forma dinâmica
     despesas.forEach (function (d) {
-
-        console.log(d)
         
         // Criando a linha (tr)
         let linha = listaDespesas.insertRow()
@@ -299,6 +302,27 @@ function carregaListaDespesas (despesas = [], filtro = false) {
         linha.insertCell(1).innerHTML = d.tipo
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+
+        // Criar o botão de exclusão
+        let btn = document.createElement("button")
+        btn.className = "btn btn-danger"
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function() {
+
+            let id = this.id.replace('id_despesa_', '')
+
+            //alert(`Despesa removida com sucesso!`)
+
+            bd.remover(id)
+
+            window.location.reload()
+
+        }
+
+        linha.insertCell(4).append(btn)
+
+        console.log(d)
 
     })
 
